bf742ccfad86879fbb9808a9b7cf61dda2e3babd
SarahSlash10 <81592096+SarahSlash10@users.noreply.github.com>
2022-10-19 16:36:16 +0200

    Add structure of pages

---
+++ resources/views/About.blade.php
@@ -0,0 +1,14 @@
+@extends("layout.app")
+@section("content")
+<div style="display:block;margin-top:10%;margin-left: auto;margin-right: auto;width: 40%;">
+    <img src="images/HeArc.png" alt="He-Arc" style="width:500px;height:200px;">
+    <div style="display:block;margin-left: auto;margin-right: auto;width: 50%;">
+        <h1 >Developpé par :</h1>
+        <ul>
+            <li>Mayer Sarah</li>
+            <li>Peck Timothy</li>
+            <li>Guyaz Loïc</li>
+        </ul>
+    </div>
+</div>
+@endsection
