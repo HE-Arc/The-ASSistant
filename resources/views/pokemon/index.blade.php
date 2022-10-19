5cd84f895163c91ce6bab4c00f35c2616d011623
Hagryn <loic.guyaz@gmail.com>
2022-10-19 16:59:37 +0200

    Create list of pokemon as cards

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -1,32 +1,84 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>Pokemons</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>HP</th>
-                <th>Attack</th>
-                <th>Defense</th>
-                <th>Special Attack</th>
-                <th>Special Defense</th>
-                <th>Speed</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pokemon as $pk)
-                <tr>
-                    <td>{{ $pk->name }}</td>
-                    <td>{{ $pk->hp }}</td>
-                    <td>{{ $pk->attack }}</td>
-                    <td>{{ $pk->defense }}</td>
-                    <td>{{ $pk->special_attack }}</td>
-                    <td>{{ $pk->special_defense }}</td>
-                    <td>{{ $pk->speed }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {!! $pokemon->links() !!}
+<h1>Pokemons</h1>
+@foreach ($pokemon as $pk)
+    <div class="row mt-12">
+        <div class="card">
+            <div class="card-header">
+                {{$pk->name}}
+            </div>
+            <div class="card-body">
+                <div class="form-row">
+                    <div class="form-group">
+                        image
+                    </div>
+                    <div class="form-col">
+                        <div class="form-row">
+                            Types
+                        </div>
+                        <div class="form-row">
+                            Type 1
+                        </div>
+                        <div class="form-row">
+                            Type 2
+                        </div>
+                    </div>
+                    <div class="form-col">
+                        <div class="form-row">
+                            PV
+                        </div>
+                        <div class="form-row">
+                            Attaque
+                        </div>
+                        <div class="form-row">
+                            Défense
+                        </div>
+                        <div class="form-row">
+                            Attaque Spé
+                        </div>
+                        <div class="form-row">
+                            Défense Spé
+                        </div>
+                        <div class="form-row">
+                            Vitesse
+                        </div>
+                    </div>
+                    <div class="form-col">
+                        <div class="form-row">
+                            {{$pk->hp}}
+                        </div>
+                        <div class="form-row">
+                            {{$pk->attack}}
+                        </div>
+                        <div class="form-row">
+                            {{$pk->defense}}
+                        </div>
+                        <div class="form-row">
+                            {{$pk->special_attack}}
+                        </div>
+                        <div class="form-row">
+                            {{$pk->special_defense}}
+                        </div>
+                        <div class="form-row">
+                            {{$pk->speed}}
+                        </div>
+                    </div>
+                    <div class="form-col">
+                        <div class="form-row">
+                            Pokepedia
+                        </div>
+                        <div class="form-row">
+                            En attaque
+                        </div>
+                        <div class="form-row">
+                            En défense
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endforeach
+{!! $pokemon->links() !!}
 @endsection
