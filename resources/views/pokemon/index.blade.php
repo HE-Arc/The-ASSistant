1d53c071cbb8d7ae1467e3d9cd1f258030d01233
Timothy Peck <timothy.peck@he-arc.ch>
2022-11-30 17:05:32 +0100

    change button icon and spacing

--- resources/views/pokemon/index.blade.php
+++ resources/views/pokemon/index.blade.php
@@ -8,6 +8,7 @@
             <div class="card mb-3">
                 <div class="card-header">
                     {{ $pk->nameWithFirstLetterCapitalized() }}
+                    <br>
                     #{{ $pk->number }}
                 </div>
                 <div class="row g-0">
@@ -124,16 +125,29 @@
                                             <a class="btn btn-info w-100 m-auto" href="{{ route('defense') }}">En
                                                 défense</a>
                                         </div>
+                                        <div class="w-100 my-2">&nbsp;</div>
                                         <div class="w-100 my-2">
                                             <form action="{{ route('pokemon.edit', $pk->id) }}" method="GET">
-                                            <button class="btn-warning" type="submit"><img src="images/edit.png" height ="40" width="40" /></button>
+                                                <button class="btn btn-info w-100 m-auto" type="submit">Éditer
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="orange" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                                                    </svg>
+                                                </button>
                                             </form>
                                         </div>
                                         <div class="w-100 my-2 ">
                                             <form action="{{ route('pokemon.destroy', $pk->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn-danger" type="submit"> <img src="images/delete.png" height ="40" width="40" /></button>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button class="btn btn-info w-100 m-auto" type="submit">Supprimer
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="#CF0000" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                                                    </svg>
+                                                </button>
                                             </form>
                                         </div>
                                     </div>
@@ -148,5 +162,6 @@
     @endforeach
     {!! $pokemon->links() !!}
 
-    <a href="{{ route('pokemon.create') }}" class="btn btn-success" style="margin-top: 15px; position:sticky; bottom:1rem;">+ Nouveau Pokémon</a>
+    <a href="{{ route('pokemon.create') }}" class="btn btn-success"
+        style="margin-top: 15px; position:sticky; bottom:1rem;">+ Nouveau Pokémon</a>
 @endsection
