daf84e5339e5add2f62a8f1e6ca892b9f3e5ad5c
Hagryn <loic.guyaz@gmail.com>
2022-11-30 18:03:49 +0100

    Get the types affected by the first type of the pokemon

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\Pokemon;
-use App\Models\PokemonType;
 use App\Models\Type;
+use App\Models\DamageType;
 
 use function PHPSTORM_META\type;
 
@@ -22,8 +21,9 @@ class TypeController extends Controller
         //$pokemon = Pokemon::find(2); // value to change
         //$pokemon = Pokemon::find(1)->types()->orderBy("updated_at")->get();//->paginate(5);
         $types = Type::all();
+        $specialDamages = DamageType::where('offensetype_id', '=', $request->type1, 'or', 'offensetype_id', '=', $request->type2)->get();
         //$pokemonX2 = PokemonType::with("types")->where("offensive_type=" == $request->type1);
-
+        //dd($specialDamages);
         return view("types.attack", ["types" => $types, "type1" => $request->type1, "type2" => $request->type2]);
 
     }
